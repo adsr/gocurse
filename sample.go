31a9db9788238077322e8b67f953742ff6c872d6
Mariano Guerra <luismarianoguerra@gmail.com>
2011-06-11 21:53:14 +0100

    fix the x/y coordinates of the '@', add description of what to do

--- sample.go
+++ sample.go
@@ -1,6 +1,6 @@
 package main
 
-import . "curses"
+import "curses"
 import "os"
 import "fmt"
 
@@ -9,26 +9,25 @@ func main() {
 	y := 10;
 	startGoCurses();
 	defer stopGoCurses();
-	
-	Init_pair(1, COLOR_RED, COLOR_BLACK);
-	
+
+	curses.Init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK);
+
 	loop(x, y);
 }
 
 func startGoCurses() {
-	Initscr();
-	if Stdwin == nil {
+	curses.Initscr();
+
+	if curses.Stdwin == nil {
 		stopGoCurses();
 		os.Exit(1);
 	}
-	
-	Noecho();
-	
-	Curs_set(CURS_HIDE);
-	
-	Stdwin.Keypad(true);
-	
-	if err := Start_color(); err != nil {
+
+	curses.Noecho();
+	curses.Curs_set(curses.CURS_HIDE);
+	curses.Stdwin.Keypad(true);
+
+	if err := curses.Start_color(); err != nil {
 		fmt.Printf("%s\n", err.String());
 		stopGoCurses();
 		os.Exit(1);
@@ -36,30 +35,37 @@ func startGoCurses() {
 }
 
 func stopGoCurses() {
-	Endwin();
+	curses.Endwin();
 }
 
 func loop(x, y int) {
+	quit := false
+
 	for {
-		Stdwin.Addstr(0, 0, "Hello,\nworld!", 0);
-		inp := Stdwin.Getch();
-		if inp == 'q' {
-			break;
-		}
-		if inp == KEY_LEFT {
+		curses.Stdwin.Addstr(0, 0, "Hello,\nworld!", 0);
+		curses.Stdwin.Addstr(3, 0, "use the cursor keys to move around", 0);
+		inp := curses.Stdwin.Getch();
+		quit = false
+
+		switch inp {
+		case 'q':
+			quit = true
+		case curses.KEY_LEFT:
 			x = x - 1;
-		}
-		if inp == KEY_RIGHT {
+		case curses.KEY_RIGHT:
 			x = x + 1;
-		}
-		if inp == KEY_UP {
+		case curses.KEY_UP:
 			y = y - 1;
-		}
-		if inp == KEY_DOWN {
+		case curses.KEY_DOWN:
 			y = y + 1;
 		}
-		Stdwin.Clear();
-		Stdwin.Addch(x, y, '@', Color_pair(1));
-		Stdwin.Refresh();
+
+		if quit {
+			break
+		}
+
+		curses.Stdwin.Clear();
+		curses.Stdwin.Addch(y, x, '@', curses.Color_pair(1));
+		curses.Stdwin.Refresh();
 	}
 }
